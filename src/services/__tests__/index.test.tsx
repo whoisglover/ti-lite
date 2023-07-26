9038cf2704032e03b74df45d4d693e192205da16
Danny Glover <djglover00@gmail.com>
2023-07-26 11:38:18 -0700

    Adding basic shape and type checking for the stations array that is returned by the API, and updating the api test

--- src/services/__tests__/index.test.tsx
+++ src/services/__tests__/index.test.tsx
@@ -2,11 +2,33 @@ import { fetchStations } from '../api'
 import { Station as StationType } from '../../types/station'
 import fetchMock from 'jest-fetch-mock'
 
+interface MockMissingName {
+  id: string
+  description: string
+  imgUrl: string
+  streamUrl: string
+  reliability: number
+  popularity: number
+  tags: string[]
+}
+
 const mockData: StationType[] = [
   {
     id: '1',
     description: 'mock station',
     name: 'mock station 1',
+    imgUrl: 'http://example.com',
+    streamUrl: 'http://example.com/stream',
+    reliability: 100,
+    popularity: 100,
+    tags: ['tag1', 'tag2'],
+  },
+]
+
+const mockMissingName: MockMissingName[] = [
+  {
+    id: '1',
+    description: 'mock station',
     imgUrl: 'http://example.com',
     streamUrl: 'http://example.com/stream',
     reliability: 100,
@@ -33,7 +55,12 @@ describe('fetchStations', () => {
 
   it('throws an error when the fetch fails', async () => {
     fetchMock.mockRejectOnce(new Error('fetch failed'))
-
     await expect(fetchStations()).rejects.toThrow('fetch failed')
+  })
+  it('throws an error when a station is missing a required field', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ data: mockMissingName }))
+    await expect(fetchStations()).rejects.toThrow(
+      'Invalid response data shape from API',
+    )
   })
 })
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,22 @@
 import { Station } from '../types/station';
 
+//future improvements
+// Env variable for API_URL constant
+// More robust error handling based on http error code (right now everything just caught in redux slice)
+// More sophisticated type checking for response body, maybe not everything required all the time
+
+
 const API_URL: string = 'https://s3-us-west-1.amazonaws.com/cdn-web.tunein.com/stations.json';
 
 export const fetchStations = async (): Promise<Station[]> => {
     try {
         const response = await fetch(API_URL);
         const responseBody = await response.json();
+        // validate the structure of the response against the Station type defined in src/types/station.ts
+        // console.log("Response body", responseBody);
+        if (!Array.isArray(responseBody.data) || !responseBody.data.every(isStation)) {
+            throw new Error('Invalid response data shape from API');
+        }
         // console.log("Fetched stations", responseBody.data)
         return responseBody.data;
     } catch (error) {
@@ -13,3 +24,17 @@ export const fetchStations = async (): Promise<Station[]> => {
         throw error;
     }
 };
+
+
+function isStation(arg: any): arg is Station {
+    return (    
+        arg.id !== undefined && typeof(arg.id) === 'string' &&
+        arg.description !== undefined && typeof(arg.description) === 'string' &&
+        arg.name !== undefined && typeof(arg.name) === 'string' &&
+        arg.imgUrl !== undefined && typeof(arg.imgUrl) === 'string' &&
+        arg.streamUrl !== undefined && typeof(arg.streamUrl) === 'string' &&
+        arg.reliability !== undefined && typeof(arg.reliability) === 'number' &&
+        arg.popularity !== undefined && typeof(arg.popularity) === 'number' &&
+        arg.tags !== undefined && Array.isArray(arg.tags) && arg.tags.every((tag: any) => typeof(tag) === 'string')
+    );
+}
